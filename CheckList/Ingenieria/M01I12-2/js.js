daddd2208a26d19d6dbb52e3b5093dcebd13d571
AndresZurita09 <141169774+AndresZurita09@users.noreply.github.com>
2024-09-26 19:49:21 +0000

    Creación de menús, M01|12-2 y M01|12-3. Se agregan estilos y tambien el selesct en js para el llenado automático. Primera revisión semanal.

---
+++ CheckList/Ingenieria/M01I12-2/js.js
@@ -0,0 +1,190 @@
+const opciones = [
+    {texto: " - Seleccione una opción - ", value:""},
+    {texto: "Completo", value:"COM"},
+    {texto: "Remplazo de parte", value:"REM"},
+    {texto: "Confirmación de Reparación", value:"CONREP" },
+    {texto: "Ajuste", value: "A" },
+    {texto: "Limpieza", value: "C"},
+    {texto: "Lubricación", value: "L"},
+    {texto: "Reparación", value: "R"},
+    {texto: "Reapretado", value: "T"},
+    {texto: "No aplica", value: "N/A"},
+    {texto: "No trabajó", value: "NT"},
+];
+
+const q1 = document.getElementById("Q1");
+const q2 = document.getElementById("Q2");
+const q3 = document.getElementById("Q3");
+const q4 = document.getElementById("Q4");
+const q5 = document.getElementById("Q5");
+const q6 = document.getElementById("Q6");
+const q7 = document.getElementById("Q7");
+const q8 = document.getElementById("Q8");
+const q9 = document.getElementById("Q9");
+const q10 = document.getElementById("Q10");
+const q11= document.getElementById("Q11");
+const q12= document.getElementById("Q12");
+const q13 = document.getElementById("Q13");
+const q14 = document.getElementById("Q14");
+const q15 = document.getElementById("Q15");
+const q16 = document.getElementById("Q16");
+const q17 = document.getElementById("Q17");
+const q18 = document.getElementById("Q18");
+const q19 = document.getElementById("Q19");
+const q20 = document.getElementById("Q20");
+const q21 = document.getElementById("Q21");
+const q22 = document.getElementById("Q22");
+const q23 = document.getElementById("Q23");
+const q24 = document.getElementById("Q24");
+const q25 = document.getElementById("Q25");
+
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q1.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q2.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q3.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q4.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q5.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q6.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q7.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q8.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q9.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q10.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q11.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q12.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q13.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q14.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q15.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q16.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q17.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q18.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q19.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q20.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q21.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q22.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q23.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q24.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q25.appendChild(optionElement);
+});
---
+++ CheckList/Ingenieria/M01|12-3/M01|12-3.js
@@ -0,0 +1,78 @@
+const opciones = [
+    {texto: " - Seleccione una opción - ", value:""},
+    {texto: "Completo", value:"COM"},
+    {texto: "Remplazo de parte", value:"REM"},
+    {texto: "Confirmación de Reparación", value:"CONREP" },
+    {texto: "Ajuste", value: "A" },
+    {texto: "Limpieza", value: "C"},
+    {texto: "Lubricación", value: "L"},
+    {texto: "Reparación", value: "R"},
+    {texto: "Reapretado", value: "T"},
+    {texto: "No aplica", value: "N/A"},
+    {texto: "No trabajó", value: "NT"},
+];
+
+const q1 = document.getElementById("Q1");
+const q2 = document.getElementById("Q2");
+const q3 = document.getElementById("Q3");
+const q4 = document.getElementById("Q4");
+const q5 = document.getElementById("Q5");
+const q6 = document.getElementById("Q6");
+const q7 = document.getElementById("Q7");
+const q8 = document.getElementById("Q8");
+const q9 = document.getElementById("Q9");
+
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q1.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q2.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q3.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q4.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q5.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q6.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q7.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q8.appendChild(optionElement);
+});
+opciones.forEach(opcion => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = opcion.texto;
+    optionElement.value = opcion.value;
+    q9.appendChild(optionElement);
+});
